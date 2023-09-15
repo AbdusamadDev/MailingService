5a0735e8c71039aaf7d26b48774d67ae30db957e
AbdusamadDev <abdusamaddev571@gmail.com>
2023-09-15 19:16:09 +0500

    Models

--- mails/models.py
+++ mails/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+import pytz as timezone
 
-# Create your models here.
+
+class Client(models.Model):
+    TIMEZONE_CHOICES = [(tz, tz) for tz in timezone.all_timezones]
+    username = models.CharField(max_length=150, null=False, unique=True, blank=False)
+    email = models.EmailField(max_length=250, null=False, unique=True, blank=False)
+    phone_number = models.IntegerField(null=False, unique=False, blank=False)
+    timezone = models.CharField(max_length=63, choices=TIMEZONE_CHOICES, default="UTC")
+
+
+class Mails(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    subject = models.CharField(max_length=255)
+    body = models.TextField()
+    status = models.CharField(max_length=50, default="pending")
+    created_at = models.DateTimeField(auto_now_add=True)
+    scheduled_at = models.DateTimeField(null=True, blank=True)
+    sent_at = models.DateTimeField(null=True, blank=True)
+
+
+# class Message(models.Model):
+#     client = models.ForeignKey(Client, on_delete=models.CASCADE)
+#     content = models.TextField()
+#     sent_status = models.CharField(max_length=50, default="pending")
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     sent_at = models.DateTimeField(null=True, blank=True)
+
