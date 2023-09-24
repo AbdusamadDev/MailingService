4ec869fe654b03ace967add30911185961854cd7
AbdusamadDev <abdusamaddev571@gmail.com>
2023-09-25 01:46:43 +0500

    Registering OpenAPI and Swagger UI

--- README.md
+++ README.md
@@ -4,8 +4,22 @@ This is Project for candidate testing task.
 Authentication is applied even if it is not mentioned on task
 Authentication is the most important and default feature of any kind of web application.
 
-Firstly, pip install all required packages with following command:
+First of all, create python virtual environment:
+python -m venv venv
+
+On linux:
+source venv/bin/activate
+
+On windows:
+.\venv\Scripts\activate
+
+Install all required packages with following command:
 pip install -r requirements.txt
+
+manage operations like migrations:
+python manage.py makemigrations
+python manage.py migrate
+
 
 In the directory called Redis there is redis executable installer for windows if 
 you are windows user, if you are linux user and love pinguins, just type this command:
@@ -29,3 +43,9 @@ $env:Path += ";C:\Program Files\Redis\" # Where your Redis root directory is loc
 run these two commands to start project:
 python manage.py runserver
 celery -A config.celery worker --pool=solo -l info
+
+
+
+API Usage:
+in the docs/ there is a nice user friendly swagger UI which allows to easily make requests
+
--- config/urls.py
+++ config/urls.py
@@ -1,22 +1,8 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 from rest_framework.routers import DefaultRouter
 
 from mails.views import (
@@ -25,13 +11,36 @@ from mails.views import (
     ClientsViewSet,
 )
 
-
 router = DefaultRouter()
 router.register(r"mails", MailingViewSet)
 router.register(r"messages", MessagesViewSet)
 router.register(r"clients", ClientsViewSet)
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Notification Service for testing",
+        default_version="v1",
+        description="This is a public openapi info",
+        terms_of_service="https://www.termsfeed.com/live/6ae1964f-78a1-4c1f-9c79-0ee628a42128",  # Just a public terms of service link
+        contact=openapi.Contact(email="abdusamaddev571@gmail.com"),
+        license=openapi.License(name="Notification Service Public Licence"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include(router.urls)),
+    re_path(
+        r"^swagger(?P<format>\.json|\.yaml)$",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    path(
+        "docs/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
